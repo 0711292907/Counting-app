61b2118635ecd13a1788cb067fc2249e073d3a14
Jason <murangira99@gmail.com>
2023-11-22 10:49:53 +0200

    export default

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import React, { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+
   const handleClick1 = () => {
     setCounter(counter + 1);
-  }; 
+  };
+
   const handleClick2 = () => {
     setCounter(counter - 1);
   };
+
   const handleClick3 = () => {
     setCounter(0);
   };
+
   return (
-    <div className="App"></div>
-    <h1>Counter App</h1>
-    <div className="counter">{counter}</div>
-    <button onClick={handleClick1}>Increase</button>
+    <div className="App">
+      <h1>Counter App</h1>
+      <div className="counter">{counter}</div>
+      <button onClick={handleClick1}>Increase</button>
       <button onClick={handleClick2}>Decrease</button>
       <button onClick={handleClick3}>Reset</button>
-      </div>
-      );
-    }
-    
-    export default App;
+    </div>
+  );
+}
+
+export default App;
